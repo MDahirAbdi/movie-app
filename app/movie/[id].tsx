3cdd02981912ed5df0e653d89157d81e9c2915ea
Mohamud Dahir <siyaasi54@gmail.com>
2025-03-24 17:59:53 +0300

    complete home, search & Movie detials

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,16 +1,130 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const MovieDetails = () => {
+import useFetch from "@/services/useFetch";
+import { fetchMovieDetails } from "@/services/api";
+import { icons } from "@/constants/icons";
+
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
+}
+
+const MovieInfo = ({ label, value }: MovieInfoProps) => (
+  <View className=" flex-col items-start justify-center mt-5">
+    <Text className=" text-light-200 font-normal text-sm">{label}</Text>
+    <Text className=" text-light-100 font-bold text-sm mt-2">
+      {value || "N/A"}
+    </Text>
+  </View>
+);
+
+const Details = () => {
   const { id } = useLocalSearchParams();
+
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string)
+  );
+
+  const formatRuntime = (minutes: number | null | undefined) => {
+    if (!minutes) return ""; // Handle null or undefined
+    const hours = Math.floor(minutes / 60);
+    const mins = minutes % 60;
+    return `${hours}h ${mins}m`;
+  };
+
+  if (loading)
+    return (
+      <SafeAreaView className="bg-primary flex-1">
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+
   return (
-    <View>
-      <Text>Movie Details: {id}</Text>
+    <View className=" bg-primary flex-1">
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: 80,
+        }}
+      >
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[550px]"
+            resizeMode="stretch"
+          />
+          <TouchableOpacity className=" absolute bottom-5 right-5 rounded-full size-14 bg-white flex items-center justify-center">
+            <Image
+              source={icons.play}
+              className=" w-6 h-7 ml-1"
+              resizeMode="stretch"
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View className=" flex-col items-start justify-center mt-5 px-5">
+          <Text className=" text-white font-bold text-xl">{movie?.title}</Text>
+          <View className=" flex-row items-center gap-x-1 mt-2">
+            <Text className=" text-light-200 text-sm">
+              {movie?.release_date?.split("-")[0]} •
+            </Text>
+
+            <Text className="text-light-200 text-sm">
+              {" "}
+              {formatRuntime(movie?.runtime)}
+            </Text>
+          </View>
+
+          <View className=" flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className=" size-4" />
+            <Text className=" text-white font-bold text-sm">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+
+            <Text className=" text-light-200 text-sm">
+              ({movie?.vote_count} votes)
+            </Text>
+          </View>
+          <MovieInfo label="OverView" value={movie?.overview} />
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g) => g.name).join(" • ") || "N/A"}
+          />
+          <View className="flex flex-row justify-between w-1/2">
+            <MovieInfo
+              label="Budget"
+              value={`$${(movie?.budget ?? 0) / 1_000_000} million`}
+            />
+            <MovieInfo
+              label="Revenue"
+              value={`$${Math.round(
+                (movie?.revenue ?? 0) / 1_000_000
+              )} million`}
+            />
+          </View>
+
+          <MovieInfo
+            label="Production Companies"
+            value={
+              movie?.production_companies?.map((c) => c.name).join(" • ") ||
+              "N/A"
+            }
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
-export default MovieDetails;
-
-const styles = StyleSheet.create({});
+export default Details;
--- services/api.ts
+++ services/api.ts
@@ -7,9 +7,11 @@ export const TMDB_CONFIG = {
   },
 };
 
-
-
-export const fetchMovies = async ({ query }: { query: string }): Promise<Movie[]> => {
+export const fetchMovies = async ({
+  query,
+}: {
+  query: string;
+}): Promise<Movie[]> => {
   const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
@@ -27,4 +29,26 @@ export const fetchMovies = async ({ query }: { query: string }): Promise<Movie[]
   return data.results;
 };
 
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
 
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
+  }
+};
